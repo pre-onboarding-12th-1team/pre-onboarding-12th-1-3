713c58f4841f4c38f6913e0abdb79d7cb8058bcd
BHyeonKim <rlaqhguse@gmail.com>
2023-09-06 10:19:40 +0900

    refactor: input저장을 redux로 옮김

--- src/components/SearchInput/index.tsx
+++ src/components/SearchInput/index.tsx
@@ -1,44 +1,41 @@
 import classNames from 'classnames/bind'
 import useDebounce from 'hooks/useDebounce'
-import {
-  ChangeEventHandler,
-  ComponentProps,
-  FC,
-  useCallback,
-  useEffect,
-  useState,
-} from 'react'
+import { ChangeEventHandler, ComponentProps, FC, useCallback, useEffect } from 'react'
+import { useAppDispatch, useAppSelector } from 'redux/hooks'
+import { changeInput, setSick } from 'redux/sickSlice'
 import sick from 'services/sick'
-import { SickResponseData } from 'types/sick'
 
 import styles from './searchInput.module.scss'
 
 const cx = classNames.bind(styles)
 
 const SearchInput: FC<ComponentProps<'input'>> = ({ ...props }) => {
-  const [value, setValue] = useState('')
-  const [_, setSicks] = useState<SickResponseData>([])
-  const fetchSick = useCallback(async (value: string) => {
-    if (!value.length) return
-    const { data } = await sick.getSick(value)
-    setSicks(data)
-  }, [])
+  const { input } = useAppSelector((state) => state.sicks)
+  const dispatch = useAppDispatch()
+  const fetchSick = useCallback(
+    async (value: string) => {
+      const { data } = await sick.getSick(value)
+      console.info('calling api')
+      dispatch(setSick(data))
+    },
+    [dispatch],
+  )
 
-  const debounce = useDebounce(fetchSick, 1000)
+  const debounce = useDebounce(fetchSick, 500)
 
   const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
-    setValue(e.currentTarget.value)
+    dispatch(changeInput(e.currentTarget.value))
   }
 
   useEffect(() => {
-    debounce(value)
-  }, [debounce, value])
+    debounce(input)
+  }, [debounce, input])
 
   return (
     <input
       className={cx('searchInput')}
       placeholder="질환명을 입력해 주세요."
-      value={value}
+      value={input}
       onChange={handleChange}
       {...props}
     />
