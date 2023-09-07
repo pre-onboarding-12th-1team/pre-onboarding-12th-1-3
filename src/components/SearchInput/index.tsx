5e33e75ee9a7c56748bd29af1854b23f0ce9cb00
HHB <bcl0206@gmail.com>
2023-09-07 18:54:07 +0900

    feat:  입력값 삭제 로직 구현
    Co-authored-by: Jieun Lee <jieeeun2@users.noreply.github.com>

--- src/components/SearchInput/index.tsx
+++ src/components/SearchInput/index.tsx
@@ -1,26 +1,39 @@
 import classNames from 'classnames/bind'
-import { ChangeEventHandler, ComponentProps, FC } from 'react'
+import { ChangeEventHandler, ComponentProps, FC, KeyboardEventHandler } from 'react'
 import { useAppDispatch, useAppSelector } from 'redux/hooks'
-import { changeInput } from 'redux/sickSlice'
+import { changeInput, selectSick } from 'redux/sickSlice'
 
 import styles from './searchInput.module.scss'
 
 const cx = classNames.bind(styles)
 
 const SearchInput: FC<ComponentProps<'input'>> = ({ ...props }) => {
-  const { input } = useAppSelector((state) => state.sicks)
+  const { input, selectedSick } = useAppSelector((state) => state.sicks)
   const dispatch = useAppDispatch()
 
   const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
     dispatch(changeInput(e.currentTarget.value))
   }
 
+  const handleDelete: KeyboardEventHandler<HTMLInputElement> = (e) => {
+    if (e.key === 'Backspace') {
+      e.preventDefault()
+      if (selectedSick) {
+        dispatch(changeInput(selectedSick.slice(0, selectedSick.length - 1)))
+        dispatch(selectSick(''))
+        return
+      }
+      dispatch(changeInput(input.slice(0, input.length - 1)))
+    }
+  }
+
   return (
     <input
       className={cx('searchInput')}
       placeholder="질환명을 입력해 주세요."
-      value={input}
+      value={selectedSick || input}
       onChange={handleChange}
+      onKeyDown={handleDelete}
       {...props}
     />
   )
