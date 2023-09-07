8965bdc80732058309b7934c902718d401c8a15b
HHB <bcl0206@gmail.com>
2023-09-07 18:53:15 +0900

    feat: submit 이벤트 로직 구현
    Co-authored-by: Jieun Lee <jieeeun2@users.noreply.github.com>
    Co-authored-by: BHyeonKim <BHyeonKim@users.noreply.github.com>

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -2,17 +2,25 @@ import classNames from 'classnames/bind'
 import SearchButton from 'components/SearchButton'
 import SearchInput from 'components/SearchInput'
 import SearchRecommendation from 'components/SearchRecommendation'
-import { useState } from 'react'
+import { FormEvent, useState } from 'react'
 
 import styles from './searchBar.module.scss'
+// import { useAppSelector } from 'redux/hooks'
 
 const cx = classNames.bind(styles)
 
 const SearchBar = () => {
+  // const { input } = useAppSelector((state) => state.sicks)
+
   const [isFocused, setIsFocused] = useState(false)
 
+  const startSearch = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    alert(e.currentTarget.querySelector('input')?.value)
+  }
+
   return (
-    <form className={cx('searchBar')}>
+    <form className={cx('searchBar')} onSubmit={startSearch}>
       <SearchInput
         onBlur={() => setIsFocused(false)}
         onFocus={() => setIsFocused(true)}
