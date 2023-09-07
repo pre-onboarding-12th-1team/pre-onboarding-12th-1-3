3e1958b52713048378fa4fbbc39fc31b61b455eb
sujikim <sujikim@student.42seoul.kr>
2023-09-07 20:21:50 +0900

    fix: 검색어가 여러번 일치하더라도 모두 하이라이트되도록 수정

--- src/utils/bold.ts
+++ src/utils/bold.ts
@@ -1,4 +1,4 @@
 import parse from 'html-react-parser'
 
 export const boldText = (input: string, keyword: string) =>
-  parse(input.replace(keyword, `<mark>${keyword}</mark>`))
+  parse(input.replaceAll(keyword, `<mark>${keyword}</mark>`))
